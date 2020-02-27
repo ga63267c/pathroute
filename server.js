c7227b3b9f5fda5db954b2eb4cb71e0e571cc9ac
ga63267c <52791904+ga63267c@users.noreply.github.com>
2020-02-27 17:44:27 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ var express = require('express'),
 var os = require('os');
 
 
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
